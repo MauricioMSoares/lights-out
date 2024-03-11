d6e1bbd0963e24922225d7a353de4718028aee61
Mauricio Massayuki <mauricio.soares@sptech.school>
2024-03-11 16:53:36 -0300

    feat: added a click counter and a restart event

--- lib/lights_out_web/live/board.ex
+++ lib/lights_out_web/live/board.ex
@@ -2,17 +2,9 @@ defmodule LightsOutWeb.Board do
   use LightsOutWeb, :live_view
 
   def mount(_params, _session, socket) do
-    grid = for x <- 0..4, y <- 0..4, into: %{}, do: {{x, y}, false}
+    grid = setup_grid()
 
-    level =
-      Enum.reduce(1..:rand.uniform(25), %{}, fn _, acc ->
-        {x, y} = {:rand.uniform(4), :rand.uniform(4)}
-        Map.put(acc, {x, y}, true)
-      end)
-
-    grid = Map.merge(grid, level)
-
-    {:ok, assign(socket, grid: grid, win: false)}
+    {:ok, assign(socket, grid: grid, win: false, clicks: 0)}
   end
 
   def handle_event("toggle", %{"x" => x, "y" => y}, socket) do
@@ -27,13 +19,30 @@ defmodule LightsOutWeb.Board do
       end)
       |> then(fn toggled_grid -> Map.merge(grid, toggled_grid) end)
 
+    clicks = socket.assigns.clicks |> increment()
     win = updated_grid |> check_win()
-    socket = assign(socket, grid: updated_grid, win: win)
+    socket = assign(socket, grid: updated_grid, win: win, clicks: clicks)
 
     case win do
       true -> {:noreply, push_event(socket, "victory", %{win: win})}
       _ -> {:noreply, socket}
     end
+  end
+
+  def handle_event("restart", _params, socket) do
+    {:noreply, assign(socket, grid: setup_grid(), win: false, clicks: 0)}
+  end
+
+  defp setup_grid do
+    grid = for x <- 0..4, y <- 0..4, into: %{}, do: {{x, y}, false}
+
+    level =
+      Enum.reduce(1..:rand.uniform(25), %{}, fn _, acc ->
+        {x, y} = {:rand.uniform(4), :rand.uniform(4)}
+        Map.put(acc, {x, y}, true)
+      end)
+
+    Map.merge(grid, level)
   end
 
   defp find_adjacent_tiles(x, y) do
@@ -50,5 +59,9 @@ defmodule LightsOutWeb.Board do
     grid
     |> Map.values()
     |> Enum.all?(fn light -> !light end)
+  end
+
+  defp increment(clicks) do
+    clicks + 1
   end
 end
