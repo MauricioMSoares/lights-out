bc335bac74885df0b3480b55e9835e74c87746f5
Mauricio Massayuki <mauricio.soares@sptech.school>
2024-03-11 14:22:11 -0300

    feat: added toggle event to buttons on the grid

--- lib/lights_out_web/live/board.ex
+++ lib/lights_out_web/live/board.ex
@@ -6,4 +6,19 @@ defmodule LightsOutWeb.Board do
 
     {:ok, assign(socket, grid: grid)}
   end
+
+  def handle_event("toggle", %{"x" => x, "y" => y}, socket) do
+    grid = socket.assigns.grid
+    grid_x = x |> String.to_integer()
+    grid_y = y |> String.to_integer()
+
+    updated_grid =
+      grid
+      |> Map.put(
+        {grid_x, grid_y},
+        !grid[{grid_x, grid_y}]
+      )
+
+    {:noreply, assign(socket, :grid, updated_grid)}
+  end
 end
